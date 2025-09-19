cf86dc0e77d706365827b0bffa892233335d741b
amakihc <tsuka_machi_0910@yahoo.co.jp>
2025-09-19 21:40:01 +0900

    add plot

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # app.py
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QComboBox, QHBoxLayout, QVBoxLayout, QLabel, QWidget
 from gui_layout import UILayout
+from data_processor import load_csv_data, compute_psd
 import sys
+import numpy as np
 
 class App(QMainWindow):
     """
@@ -10,11 +12,97 @@ class App(QMainWindow):
     """
     def __init__(self):
         super().__init__()
-        # UILayoutクラスのインスタンスを作成
         self.ui = UILayout()
-        # メインウィンドウにUIを設定
         self.setCentralWidget(self.ui)
         self.setGeometry(100, 100, 1200, 600)
+
+        self.df = None
+        self.sampling_rate = 0
+        
+        # UIコンポーネントをAppクラスに追加
+        self.ui.browse_button.clicked.connect(self.browse_file)
+        
+        # ドロップダウンリストを追加
+        self.channel_selection_layout = QHBoxLayout()
+        self.channel_label = QLabel("チャンネルを選択:")
+        self.channel_combo_box = QComboBox()
+        self.channel_combo_box.currentIndexChanged.connect(self.plot_selected_channel)
+        
+        self.channel_selection_layout.addWidget(self.channel_label)
+        self.channel_selection_layout.addWidget(self.channel_combo_box)
+        
+        self.ui.main_layout.insertLayout(1, self.channel_selection_layout) # ファイル選択の下に挿入
+
+    def browse_file(self):
+        """
+        ファイルダイアログを開き、CSVファイルを選択する
+        """
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "CSVファイルを選択", "", "CSV Files (*.csv)"
+        )
+        if file_path:
+            self.ui.file_path_label.setText(file_path)
+            self.process_file(file_path)
+
+    def process_file(self, file_path):
+        """
+        ファイルを処理し、ドロップダウンリストを更新する
+        """
+        self.df, self.sampling_rate = load_csv_data(file_path)
+        if self.df is None or self.df.empty:
+            return
+
+        # ドロップダウンリストを更新
+        self.channel_combo_box.clear()
+        
+        # 最初の列は時間データなので、2列目からドロップダウンリストに追加
+        num_columns = len(self.df.columns)
+        channel_names = [f"列 {i+1}" for i in range(1, num_columns)]
+        self.channel_combo_box.addItems(channel_names)
+
+        # 最初のチャンネルのデータを自動でプロット
+        self.plot_selected_channel()
+    
+    def plot_selected_channel(self):
+        """
+        ドロップダウンリストで選択されたチャンネルのデータをプロットする
+        """
+        selected_index = self.channel_combo_box.currentIndex()
+        
+        # インデックスが有効か確認 (ドロップダウンリストが空でないか)
+        if selected_index < 0 or self.df is None:
+            return
+
+        # 選択された列のインデックス（0ベース）を取得
+        # 最初の列は時間データなので、選択されたインデックスに1を足す
+        data_column_index = selected_index + 1
+        
+        signal_data = self.df.iloc[:, data_column_index].values
+        time_data = self.df.iloc[:, 0].values
+
+        # データをプロット
+        self.plot_time_series(time_data, signal_data)
+        self.plot_psd(signal_data)
+        
+    def plot_time_series(self, time_data, signal_data):
+        """
+        時系列データをグラフにプロットする
+        """
+        self.ui.time_series_plot.clear()  # 既存のプロットをクリア
+        # プロットの色を青に設定
+        self.ui.time_series_plot.plot(time_data, signal_data, pen='b')
+        
+    def plot_psd(self, data):
+        """
+        PSDを計算し、グラフにプロットする
+        """
+        frequencies, psd = compute_psd(data, self.sampling_rate)
+        
+        self.ui.psd_plot.clear()  # 既存のプロットをクリア
+        if len(frequencies) > 0 and len(psd) > 0:
+            # プロットの色を青に設定
+            self.ui.psd_plot.plot(frequencies, psd, pen='b')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- gui_layout.py
+++ gui_layout.py
@@ -2,6 +2,7 @@
 
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QHBoxLayout, QLabel
 import pyqtgraph as pg
+from PyQt5.QtGui import QFont
 
 class UILayout(QWidget):
     """
@@ -27,18 +28,58 @@ class UILayout(QWidget):
         self.file_path_label = QLabel("ファイルが選択されていません")
 
         # グラフウィジェット
+        # 全てのグラフに適用される設定
         pg.setConfigOption('background', 'w')
         pg.setConfigOption('foreground', 'k')
+
+        # フォントオブジェクトを作成
+        font = QFont()
+        font.setPointSize(20) # 10ptに設定
 
         self.time_series_plot = pg.PlotWidget()
         self.time_series_plot.setTitle("時系列プロット")
         self.time_series_plot.setLabel('bottom', '時間')
         self.time_series_plot.setLabel('left', '振幅')
+        # グリッドを灰色で有効化
+        self.time_series_plot.showGrid(x=True, y=True, alpha=0.3)
+        # グラフの色を白黒に設定
+        self.time_series_plot.setBackground('w')
+        
+        # 目盛りと軸ラベルのスタイル設定
+        self.time_series_plot.getPlotItem().getAxis('bottom').setPen('k')
+        self.time_series_plot.getPlotItem().getAxis('left').setPen('k')
+        
+        # 軸ラベルのフォントサイズを設定
+        self.time_series_plot.getPlotItem().getAxis('bottom').label.setFont(font)
+        self.time_series_plot.getPlotItem().getAxis('left').label.setFont(font)
+
+        # 目盛り数字のフォントサイズを設定
+        self.time_series_plot.getPlotItem().getAxis('bottom').setTickFont(font)
+        self.time_series_plot.getPlotItem().getAxis('left').setTickFont(font)
+
 
         self.psd_plot = pg.PlotWidget()
         self.psd_plot.setTitle("パワースペクトル密度 (PSD)")
         self.psd_plot.setLabel('bottom', '周波数 (Hz)')
-        self.psd_plot.setLabel('left', 'パワースペクトル密度')
+        self.psd_plot.setLabel('left', 'ASD')
+        # グリッドを灰色で有効化
+        self.psd_plot.showGrid(x=True, y=True, alpha=0.3)
+        # 対数スケールに変更
+        self.psd_plot.setLogMode(x=True, y=True)
+        # グラフの色を白黒に設定
+        self.psd_plot.setBackground('w')
+
+        # 目盛りと軸ラベルのスタイル設定
+        self.psd_plot.getPlotItem().getAxis('bottom').setPen('k')
+        self.psd_plot.getPlotItem().getAxis('left').setPen('k')
+
+        # 軸ラベルのフォントサイズを設定
+        self.psd_plot.getPlotItem().getAxis('bottom').label.setFont(font)
+        self.psd_plot.getPlotItem().getAxis('left').label.setFont(font)
+
+        # 目盛り数字のフォントサイズを設定
+        self.psd_plot.getPlotItem().getAxis('bottom').setTickFont(font)
+        self.psd_plot.getPlotItem().getAxis('left').setTickFont(font)
 
     def add_widgets_to_layout(self):
         """
